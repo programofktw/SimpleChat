ad1fa421891e8742ec0bf9bb7b29710fe649a733
programofktw <thkim000317@naver.com>
2025-02-28 11:33:21 +0900

    first project : simplechat v1

---
+++ src/ClientConsole.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import client.*;
+import common.*;
+
+public class ClientConsole implements ChatIF{
+    final public static int DEFAULT_PORT = 5555;
+
+    ChatClient client;
+
+    public ClientConsole(String host, int port){
+        try{
+            client = new ChatClient(host, port, this);
+        }catch(IOException e){
+            System.out.println("Error : Can't setup connection! Termination client.");
+            System.exit(1);
+        }
+
+    }
+
+    public void accept()
+    {
+        try {
+            BufferedReader fromConsole = new BufferedReader(new InputStreamReader(System.in));
+
+            String message;
+
+            while(true){
+                message = fromConsole.readLine();
+                clinet.handleMessageFromClientUI(message);
+            }
+        } catch(Exception ex){
+            System.out.println("Unexpected error while reading form console!");
+        }
+    }
+
+
+    public void display(String message){
+        System.out.println("> "+message);
+    }
+
+    public static void main(String[] args){
+        String host = "";
+        int port = 0;
+
+        try{
+            host = args[0];
+        } catch(ArrayIndexOutOfBoundsException e){
+            host = "localhost";
+        }
+
+        ClientConsole chat = new ClientConsole(host, DEFAULT_PORT);
+        chat.accept();
+    }
+}
---
+++ src/client/ChatClient.java
@@ -0,0 +1,41 @@
+package client;
+
+import osrf.client.*;
+import common.*;
+import java.io.*;
+
+public class ChatClient extends AbstractClient
+{
+    ChatIF clientUI;
+
+    public ChatClient(String host, int port, ChatIF clientUI) throws IOException
+    {
+        super(host, port);
+        this.clientUI = clientUI;
+        openConnection();
+    }
+
+    public void handleMessageFromServer(Object msg)
+    {
+        clinet.display(msg.toString());
+    }
+
+    public void handleMessageFromClientUI(String message)
+    {
+        try{
+            sendToServer(message);
+        } catch(IOException e){
+            clientUI.display("could not send message to server. Termination client.");
+            quit();
+        }
+
+    }
+
+    public void quit()
+    {
+        try {
+            closeConnection();
+        }catch(IOException e){}
+        System.exit(0);
+    }
+}
